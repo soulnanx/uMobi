8361d6b2ffb8c604e3b0313213aebfcca1ddb718
renan <rsantos.dev@gmail.com>
2017-07-21 19:19:10 -0300

    fluxo de novo local
    - tela de cadastro
    - tela de review
    - tela de finalização

--- app/src/main/java/br/com/umobi/ui/activity/NewPlaceActivity.java
+++ app/src/main/java/br/com/umobi/ui/activity/NewPlaceActivity.java
@@ -1,9 +1,12 @@
 package br.com.umobi.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 
 import br.com.umobi.R;
+import br.com.umobi.ui.fragment.NewPlaceBasicDataFragment;
+import butterknife.ButterKnife;
 
 public class NewPlaceActivity extends BaseAppCompatActivity {
 
@@ -11,5 +14,28 @@ public class NewPlaceActivity extends BaseAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_place);
+
+        init();
+    }
+
+    private void init() {
+        ButterKnife.bind(this);
+
+        changeFragment(new NewPlaceBasicDataFragment(), NewPlaceBasicDataFragment.TAG);
+    }
+
+    public void changeFragment(Fragment frag, String tag) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit);
+        transaction.replace(R.id.activity_new_place_fragments, frag, tag);
+
+//        if (frag.getTag().equals(this.tag)) {
+//            transaction.disallowAddToBackStack();
+//        } else {
+//            transaction.addToBackStack(null);
+//        }
+
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/br/com/umobi/ui/fragment/NewPlaceFinishFragment.java
@@ -0,0 +1,52 @@
+package br.com.umobi.ui.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import br.com.umobi.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class NewPlaceFinishFragment extends Fragment {
+
+    public static final String TAG = "NewPlaceFinishFragment";
+
+    @BindView(R.id.fragment_new_place_review_next)
+    Button next;
+
+    private View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_new_place_finish, container, false);
+        init();
+        return view;
+    }
+
+    private void init() {
+        ButterKnife.bind(this, view);
+
+        setEvents();
+    }
+
+    private void setEvents() {
+        next.setOnClickListener(onClickNext());
+    }
+
+    private View.OnClickListener onClickNext() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NewPlaceFinishFragment.this.getActivity().finish();
+            }
+        };
+    }
+
+}
